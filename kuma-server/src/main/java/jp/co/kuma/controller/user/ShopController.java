ceb6cf209dfec131ed6ea8cae79caacea4b164d2
wangyingxin <shintokyo2024@gmail.com>
2025-08-11 07:45:35 +0900

    feat: ユーザー側の店の営業状態を取得するShopControllerを追加

---
+++ kuma-server/src/main/java/jp/co/kuma/controller/user/ShopController.java
@@ -0,0 +1,27 @@
+package jp.co.kuma.controller.user;
+
+import jp.co.kuma.result.Result;
+import jp.co.kuma.service.ShopService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@RequiredArgsConstructor
+public class ShopController {
+    
+    private final ShopService shopService;
+    
+    /**
+     * 店の営業状態を取得します。
+     */
+    @GetMapping("/status")
+    public Result<String> getStatus() {
+        String status = shopService.getStatus();
+        return Result.success(status);
+    }
+}
